44d8b7156250b7f3d6799c45391c884281cd5c79
Hakim <hakimalhaddad2@gmail.com>
2025-02-03 10:02:56 +0300

    Made functional passwrod yippe

---
+++ controllers/auth.js
@@ -0,0 +1,28 @@
+const User = require('../models/user')
+
+
+
+const signUp = (req, res) => {
+    res.render('auth/sign-up.ejs', 
+        {title: 'Sign up', msg: ''} )
+}
+
+const addUser = async (req, res) => {
+    console.log('request body:', req.body)
+    const userInDatabase = await User.findOne({ username: req.body.username})
+    if (userInDatabase) {
+        return res.render('auth/sign-up.ejs', {
+            title: 'Sign up',
+            msg: 'Username already taken.'
+        })
+    }
+
+    const user = await User.create(req.body)
+    console.log('new user: ', user)
+
+}
+
+module.exports = {
+    signUp,
+    addUser,
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const morgan = require('morgan')
+const path = require('path')
 
 const port = process.env.PORT ? process.env.PORT : '3000'
 
@@ -17,13 +18,19 @@ mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
 
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")));
 
 //CONTROLLERS
 const pagesCtrl = require('./controllers/pages')
+const authCtrl = require('./controllers/auth')
 
 
 //ROUTE HANDLERS
 app.get('/', pagesCtrl.home)
+app.get('/auth/sign-up', authCtrl.signUp)
+app.post('/auth/sign-up', authCtrl.addUser)
 
 
 app.listen(port, () => {
